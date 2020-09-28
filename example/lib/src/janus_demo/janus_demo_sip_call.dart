d0c09aab60a443ae8503c72d44058c99cae133af
Maanas Royy <m4manas@gmail.com>
2020-09-28 10:46:44 +0800

    Added work in progress sip call example

--- example/lib/src/janus_demo/janus_demo_sip_call.dart
+++ example/lib/src/janus_demo/janus_demo_sip_call.dart
@@ -13,73 +13,119 @@ class JanusSipCall extends StatefulWidget {
 class _JanusSipCallState extends State<JanusSipCall> {
   String server = "wss://janutter.tzty.net:7007";
   // String server = "https://janutter.tzty.net:8008/janus";
-  var sipcall;
   String opaqueId = "siptest-" + Janus.randomString(12);
 
-  String selectAppraoch;
-  bool registered = false;
-  var masterId;
-  var helpers = {};
-  int helpersCount;
-  var incoming;
+  bool audioEnabled = false;
+  bool videoEnabled = false;
+
+  String myUsername;
+  String yourUsername;
+  Map<String, dynamic> peers;
+
+  bool doSimulcast = false;
+  bool doSimulcast2 = false;
+  bool simulcastStarted = false;
 
   Session session;
-  Plugin sip;
+  Plugin sipcall;
 
   MediaStream _localStream;
   RTCVideoRenderer _localRenderer = new RTCVideoRenderer();
   RTCVideoRenderer _remoteRenderer = new RTCVideoRenderer();
   bool _inCalling = false;
+  bool _registered = false;
+  var selectedApproach;
+  var masterId;
+  var helpers = {};
+  int helpersCount = 0;
 
-  TextEditingController nameController = TextEditingController();
+  TextEditingController textController = TextEditingController();
 
   _JanusSipCallState({Key key});
 
   @override
   void initState() {
     super.initState();
-    Janus.init(options: {"debug": "all"}, callback: null);
+    Janus.init(options: {"debug": "all"}, callback: _connect);
     initRenderers();
   }
 
   initRenderers() async {
     await _localRenderer.initialize();
     await _remoteRenderer.initialize();
-    _connect();
   }
 
   @override
   void deactivate() {
     super.deactivate();
-    if (session != null) session.destroy();
     _localRenderer.dispose();
     _remoteRenderer.dispose();
+    if (session != null) session.destroy();
   }
 
   registerDialog() {
     showDialog(
         context: context,
-        barrierDismissible: false,
-        child: AlertDialog(
-          title: Text("Register as"),
-          content: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: "Username"),
-                controller: nameController,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)), //this right here
+            child: Container(
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Register as username ...'),
+                      controller: textController,
+                    ),
+                    SizedBox(
+                      width: 320.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          registerUsername(textController.text);
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          "Register",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.green,
+                      ),
+                    )
+                  ],
+                ),
               ),
-              RaisedButton(
-                color: Colors.green,
-                textColor: Colors.white,
-                onPressed: () {
-                  registerUsername(nameController.text);
-                },
-                child: Text("Proceed"),
-              )
-            ],
-          ),
-        ));
+            ),
+          );
+        });
+  }
+
+  showAlert(String title, String text) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text(title),
+          content: new Text(text),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   makeCallDialog() {
@@ -87,54 +133,89 @@ class _JanusSipCallState extends State<JanusSipCall> {
         context: context,
         barrierDismissible: false,
         child: AlertDialog(
-          title: Text("Call Registered User or wait for user to call you"),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text("Call"),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              Text("Wait for user to call you."),
               TextFormField(
-                decoration: InputDecoration(
-                    labelText: "Name Of Registered User to call"),
-                controller: nameController,
+                decoration: InputDecoration(labelText: "or Call user"),
+                controller: textController,
               ),
               RaisedButton(
                 color: Colors.green,
                 textColor: Colors.white,
                 onPressed: () {
-                  doCall(nameController.text, false, null);
+                  doCall(textController.text);
                   Navigator.of(context).pop();
                 },
                 child: Text("Call"),
-              )
+              ),
+              new FlatButton(
+                child: new Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
             ],
           ),
         ));
   }
 
+  answerCallDialog(jsep) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Incoming call'),
+          content: Text("Incoming call from " + yourUsername + "!"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Accept"),
+              onPressed: () {
+                answerCall(jsep);
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text("Decline"),
+              onPressed: () {
+                declineCall();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   registerUsername(username) {
     if (sipcall != null) {
-      sipcall.send({
-        "message": {"request": "register", "username": username}
-      });
+      Callbacks callbacks = Callbacks();
+      callbacks.message = {"request": "register", "username": username};
+      sipcall.send(callbacks);
     }
   }
 
-  doCall(uri, doVideo, referId) {
+  doCall(String username) {
     if (sipcall != null) {
       Callbacks callbacks = Callbacks();
-      callbacks.media = {
-        "audioSend": true,
-        "audioRecv": true,
-        "videoSend": doVideo,
-        "videoRecv": doVideo
-      };
+      callbacks.media["data"] = false;
+      callbacks.simulcast = doSimulcast;
+      callbacks.simulcast2 = doSimulcast2;
       callbacks.success = (RTCSessionDescription jsep) {
         Janus.debug("Got SDP!");
         Janus.debug(jsep.toMap());
-        Map<String, dynamic> body = {"request": "call", "uri": uri};
-        if (referId) body["refer_id"] = referId;
+        Map<String, dynamic> body = {"request": "call", "username": username};
         callbacks.message = body;
         callbacks.jsep = jsep.toMap();
         sipcall.send(callbacks);
+        setState(() {
+          _inCalling = true;
+        });
       };
       callbacks.error = (error) {
         Janus.error("WebRTC error:", error);
@@ -145,6 +226,180 @@ class _JanusSipCallState extends State<JanusSipCall> {
     }
   }
 
+  answerCall(jsep) {
+    Janus.debug(jsep.toString());
+    Callbacks callbacks = Callbacks();
+    callbacks.jsep = jsep;
+    callbacks.media["data"] = false;
+    callbacks.media["video"] = false;
+    callbacks.simulcast = doSimulcast;
+    callbacks.simulcast2 = doSimulcast2;
+    callbacks.success = (RTCSessionDescription jsep) {
+      Janus.debug("Got SDP!");
+      Janus.debug(jsep.toMap());
+      callbacks.message = {"request": "accept"};
+      callbacks.jsep = jsep.toMap();
+      sipcall.send(callbacks);
+      setState(() {
+        _inCalling = true;
+      });
+    };
+    callbacks.error = (error) {
+      Janus.error("WebRTC error:", error);
+      Janus.log("WebRTC error... " + jsonEncode(error));
+    };
+    sipcall.createAnswer(callbacks);
+  }
+
+  declineCall() {
+    Janus.log("Decline call pressed");
+  }
+
+  updateCall(jsep) {
+    Callbacks callbacks = Callbacks();
+    callbacks.jsep = jsep;
+    if (jsep.type == 'answer') {
+      sipcall.handleRemoteJsep(callbacks);
+    } else {
+      callbacks.media["data"] = false;
+      callbacks.simulcast = doSimulcast;
+      callbacks.simulcast2 = doSimulcast2;
+      callbacks.success = (RTCSessionDescription jsep) {
+        Janus.debug("Got SDP!");
+        Janus.debug(jsep.toMap());
+        callbacks.message = {"request": "set"};
+        callbacks.jsep = jsep.toMap();
+        sipcall.send(callbacks);
+      };
+      callbacks.error = (error) {
+        Janus.error("WebRTC error:", error);
+        Janus.log("WebRTC error... " + jsonEncode(error));
+      };
+      sipcall.createAnswer(callbacks);
+    }
+  }
+
+  _onMessage(msg, jsep) {
+    Janus.debug(" ::: Got a message :::");
+    Janus.debug(msg);
+    Map<String, dynamic> result = msg["result"];
+    if (result != null) {
+      if (result["list"] != null) {
+        peers = result["list"];
+        Janus.debug("Got a list of registered peers:");
+        Janus.debug(peers.toString());
+      } else if (result["event"] != null) {
+        String event = result["event"];
+        if (event == 'registered') {
+          setState(() {
+            _registered = true;
+          });
+
+          myUsername = result["username"];
+          Janus.log("Successfully registered as " + myUsername + "!");
+          showAlert(
+              "Registered", "Successfully registered as " + myUsername + "!");
+          // Get a list of available peers, just for fun
+          // TODO Enable buttons to call now
+        } else if (event == 'calling') {
+          Janus.log("Waiting for the peer to answer...");
+          // TODO Any ringtone?
+          showAlert('Calling', "Waiting for the peer to answer...");
+        } else if (event == 'incomingcall') {
+          Janus.log("Incoming call from " + result["username"] + "!");
+          yourUsername = result["username"];
+          // Notify user
+          answerCallDialog(jsep);
+        } else if (event == 'accepted') {
+          if (result["username"] == null) {
+            Janus.log("Call started!");
+          } else {
+            yourUsername = result["username"];
+            Janus.log(yourUsername + " accepted the call!");
+          }
+          // Video call can start
+          if (jsep != null) {
+            Callbacks callbacks = Callbacks();
+            callbacks.jsep = jsep;
+            sipcall.handleRemoteJsep(callbacks);
+          }
+        } else if (event == 'update') {
+          if (jsep != null) {
+            updateCall(jsep);
+          }
+        } else if (event == 'hangup') {
+          Janus.log("Call hung up by " +
+              result["username"] +
+              " (" +
+              result["reason"] +
+              ")!");
+          sipcall.hangup(false);
+          _hangUp();
+        }
+      }
+    } else {
+      var error = msg["error"];
+      showAlert("Error", error.toString());
+      _hangUp();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Videocall Test'),
+        actions: <Widget>[],
+      ),
+      body: new OrientationBuilder(
+        builder: (context, orientation) {
+          return new Center(
+            child: new Container(
+              decoration: new BoxDecoration(color: Colors.white),
+              child: new Stack(
+                children: <Widget>[
+                  new Align(
+                    alignment: orientation == Orientation.portrait
+                        ? const FractionalOffset(0.5, 0.1)
+                        : const FractionalOffset(0.0, 0.5),
+                    child: new Container(
+                      margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                      width: 320.0,
+                      height: 240.0,
+                      child: new RTCVideoView(_localRenderer),
+                      decoration: new BoxDecoration(color: Colors.black54),
+                    ),
+                  ),
+                  new Align(
+                    alignment: orientation == Orientation.portrait
+                        ? const FractionalOffset(0.5, 0.9)
+                        : const FractionalOffset(1.0, 0.5),
+                    child: new Container(
+                      margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                      width: 320.0,
+                      height: 240.0,
+                      child: new RTCVideoView(_remoteRenderer),
+                      decoration: new BoxDecoration(color: Colors.black54),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _registered
+            ? (_inCalling ? _hangUp : makeCallDialog)
+            : registerDialog,
+        tooltip: _registered ? (_inCalling ? 'Hangup' : 'Call') : 'Register',
+        child: new Icon(_registered
+            ? (_inCalling ? Icons.call_end : Icons.phone)
+            : Icons.verified_user),
+      ),
+    );
+  }
+
   void _connect() async {
     GatewayCallbacks gatewayCallbacks = GatewayCallbacks();
     gatewayCallbacks.server = this.server;
@@ -152,16 +407,13 @@ class _JanusSipCallState extends State<JanusSipCall> {
     gatewayCallbacks.error = (error) => Janus.log(error.toString());
     gatewayCallbacks.destroyed = () => deactivate();
     Session(gatewayCallbacks); // async httpd call
-    setState(() {
-      _inCalling = true;
-    });
   }
 
   void _attach(int sessionId) {
     session = Janus.sessions[sessionId.toString()];
 
     Callbacks callbacks = Callbacks();
-    callbacks.plugin = "janus.plugin.videocall";
+    callbacks.plugin = "janus.plugin.sip";
     callbacks.opaqueId = opaqueId;
     callbacks.success = _success;
     callbacks.error = _error;
@@ -186,13 +438,6 @@ class _JanusSipCallState extends State<JanusSipCall> {
         ", id=" +
         sipcall.getId().toString() +
         ")");
-    Map<String, dynamic> body = {"audio": true, "video": true};
-    Janus.debug("Sending message (" + jsonEncode(body) + ")");
-    // Create am empty callback for the message
-    Callbacks callbacks = Callbacks();
-    callbacks.message = body;
-    sipcall.send(callbacks);
-    // No media provided: by default, it's sendrecv for audio and video
   }
 
   _error(error) {
@@ -224,32 +469,6 @@ class _JanusSipCallState extends State<JanusSipCall> {
         " packets on this PeerConnection (" +
         lost +
         " lost packets)");
-  }
-
-  _onMessage(msg, jsep) {
-    Janus.debug(" ::: Got a message :::");
-    Janus.debug(msg);
-    if (jsep != null) {
-      Janus.debug("Handling SDP as well...");
-      Janus.debug(jsep);
-      Callbacks callbacks = Callbacks();
-      callbacks.jsep = jsep;
-      sipcall.handleRemoteJsep(callbacks);
-    }
-    var result = msg["result"];
-    Janus.log(result);
-    if (result != null) {
-      if (result == "done") {
-        Janus.log("Echo Test is over");
-      }
-
-      var status = result["status"];
-      if (status == "slow_link") {
-        Janus.log(
-            "Janus apparently missed many packets we sent, maybe we should reduce the bitrate",
-            "Packet loss?");
-      }
-    }
   }
 
   _onLocalStream(MediaStream stream) {
@@ -294,67 +513,7 @@ class _JanusSipCallState extends State<JanusSipCall> {
     Janus.log('Switching camera');
   }
 
-  _muteMic() {}
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Sipcall Test'),
-        actions: <Widget>[
-          Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: registerDialog,
-                child: Icon(
-                  Icons.settings,
-                  size: 26.0,
-                ),
-              )),
-        ],
-      ),
-      body: new OrientationBuilder(
-        builder: (context, orientation) {
-          return new Center(
-            child: new Container(
-              decoration: new BoxDecoration(color: Colors.white),
-              child: new Stack(
-                children: <Widget>[
-                  new Align(
-                    alignment: orientation == Orientation.portrait
-                        ? const FractionalOffset(0.5, 0.1)
-                        : const FractionalOffset(0.0, 0.5),
-                    child: new Container(
-                      margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-                      width: 320.0,
-                      height: 240.0,
-                      child: new RTCVideoView(_localRenderer),
-                      decoration: new BoxDecoration(color: Colors.black54),
-                    ),
-                  ),
-                  new Align(
-                    alignment: orientation == Orientation.portrait
-                        ? const FractionalOffset(0.5, 0.9)
-                        : const FractionalOffset(1.0, 0.5),
-                    child: new Container(
-                      margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-                      width: 320.0,
-                      height: 240.0,
-                      child: new RTCVideoView(_remoteRenderer),
-                      decoration: new BoxDecoration(color: Colors.black54),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _inCalling ? _hangUp : doCall,
-        tooltip: _inCalling ? 'Hangup' : 'Call',
-        child: new Icon(_inCalling ? Icons.call_end : Icons.phone),
-      ),
-    );
+  _muteMic() {
+    Janus.log('Mute mic.');
   }
 }
